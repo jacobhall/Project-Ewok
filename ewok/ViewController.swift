74daccd12b894a115e757e735f59dbc3912c30c6
jacobhall <jacob.hall1@maine.edu>
2016-08-02 11:52:36 -0400

    Initial Commit

---
+++ ewok/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ewok
+//
+//  Created by Jacob Hall on 8/2/16.
+//  Copyright © 2016 ASAP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
